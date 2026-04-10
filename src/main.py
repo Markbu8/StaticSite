8290ffeb1d66dff02cd77275139d6026f897a4c8
Mark Buckley <mbmarkbu8@gmail.com>
2026-04-09 21:22:21 -0600

    no jekyll

--- src/main.py
+++ src/main.py
@@ -2,10 +2,14 @@ from htmlnode import HTMLNode,LeafNode,ParentNode
 from textnode import TextNode,TextType,text_node_to_html_node,split_nodes_delimiter,extract_markdown_images
 from copystatic import *
 from pages import *
+import sys
 
 def main():
+    base_path = "/"
+    if len(sys.argv) > 1:
+        base_path = sys.argv[1]
     copy_static("./static","./public")
-    generate_pages_recursive("./content/","template.html","public/")
+    generate_pages_recursive(f"content/","template.html",base_path,"docs/")
     
 if __name__ == "__main__":
     main()
--- src/pages.py
+++ src/pages.py
@@ -8,18 +8,19 @@ def extract_title(markdown):
             return line[2:]
     raise Exception("no heading")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path,base_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     markdown = open(from_path).read()
     template = open(template_path).read()
     html_string = markdown_to_html_node(markdown).to_html()
     title = extract_title(markdown)
     new_html = template.replace("{{ Title }}",title).replace("{{ Content }}",html_string)
+    new_html = new_html.replace('href="/',f'href="{base_path}').replace('src="/',f'src="{base_path}')
     os.makedirs(os.path.dirname(dest_path),exist_ok=True)
     dest_file = open(dest_path,mode="w")
     dest_file.write(new_html) 
     
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, base_path, dest_dir_path):
     file_folder_list = os.listdir(dir_path_content)
     
     for f_or_f in file_folder_list:
@@ -27,6 +28,6 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         dest_path = os.path.join(dest_dir_path,f_or_f)
         if os.path.isfile(src_path):
             if src_path[-3:] == ".md":
-                generate_page(src_path,template_path,dest_path.replace(".md",".html"))
+                generate_page(src_path,template_path,base_path,dest_path.replace(".md",".html"))
         elif os.path.isdir(src_path):
-            generate_pages_recursive(src_path,template_path,dest_path)
+            generate_pages_recursive(src_path,template_path,base_path,dest_path)
