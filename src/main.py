545a9bacbd9863e2f8f8c27068f555b529ea6faa
Mark Buckley <mbmarkbu8@gmail.com>
2026-04-09 21:29:46 -0600

    forgot images

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ def main():
     base_path = "/"
     if len(sys.argv) > 1:
         base_path = sys.argv[1]
-    copy_static("./static","./public")
+    copy_static("./static","./docs")
     generate_pages_recursive(f"content/","template.html",base_path,"docs/")
     
 if __name__ == "__main__":
